62bae810b2ecfb60531763f385b929bac29747cf
David Maitland <David Maitland>
2016-04-19 22:10:16 +0100

    Added Angular project

---
+++ angularjs-1.4.0/app/app.js
@@ -0,0 +1,88 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('myApp', [
+  'ngRoute',
+  'myApp.meal.list',
+  'myApp.view2'
+])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.otherwise({redirectTo: '/meal/list'});
+}])
+
+.config(['$httpProvider', function($httpProvider) {
+     $httpProvider.interceptors.push(function($q) {
+        return {
+          responseError: function(rejection) {
+                if(rejection.status <= 0) {
+                    rejection.status = 500;
+                    rejection.statusText = 'Server unreachable';
+                }
+                return $q.reject(rejection);
+            }
+        };
+    });
+}])
+
+.directive('modal', function () {
+    return {
+      template: '<div class="modal fade">' +
+          '<div class="modal-dialog">' +
+            '<div class="modal-content">' +
+              '<div class="modal-header">' +
+                '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' +
+                '<h4 class="modal-title">{{ title }}</h4>' +
+              '</div>' +
+              '<div class="modal-body" ng-transclude></div>' +
+            '</div>' +
+          '</div>' +
+        '</div>',
+      restrict: 'E',
+      transclude: true,
+      replace:true,
+      scope:true,
+      link: function postLink(scope, element, attrs) {
+        scope.title = attrs.title;
+
+        scope.$watch(attrs.visible, function(value){
+          if(value == true)
+            $(element).modal('show');
+          else
+            $(element).modal('hide');
+        });
+
+        $(element).on('shown.bs.modal', function(){
+          scope.$apply(function(){
+            scope.$parent[attrs.visible] = true;
+          });
+        });
+
+        $(element).on('hidden.bs.modal', function(){
+          scope.$apply(function(){
+            scope.$parent[attrs.visible] = false;
+          });
+        });
+      }
+    };
+})
+
+/*
+.controller('MainCtrl', ['$scope', '$timeout', function($scope, $timeout) {
+    $scope.successAlertMessage = null;
+    $scope.errorAlertMessage = null;
+    var mainPageScope = $scope;
+    this.showSuccess = function(message)
+    {
+        mainPageScope.successAlertMessage = message;
+        $timeout(function(){mainPageScope.successAlertMessage = null;}, 2000);
+    };
+
+    this.showError = function(message)
+    {
+        mainPageScope.errorlertMessage = message;
+        $timeout(function(){mainPageScope.errorAlertMessage = null;}, 2000);
+    };
+}])
+*/
+;
---
+++ angularjs-1.4.0/app/meal/list/meal-list.js
@@ -0,0 +1,143 @@
+'use strict';
+
+angular.module('myApp.meal.list', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/meal/list', {
+    templateUrl: 'meal/list/meal-list.html',
+    controller: 'MealListCtrl'
+  });
+}])
+
+.controller('MealListCtrl', ['$scope', '$timeout', 'MealsSvc', function($scope, $timeout, mealsService) {
+
+    $scope.meal = null;
+    $scope.meals = [];
+    $scope.successMessage = null;
+    $scope.errorMessage = null;
+    $scope.showModal = false;
+
+    $scope.toggleModal = function()
+    {
+        $scope.showModal = !$scope.showModal;
+    };
+
+    $scope.getMeals = function()
+    {
+        mealsService.getMeals().then(function(serviceResponse) {
+            $scope.meals = serviceResponse.data;
+            $scope.errorMessage = serviceResponse.message;
+            $timeout(function(){$scope.errorMessage = null;}, 2000);
+        });
+    };
+
+    $scope.getCurrentMeal = function()
+    {
+        return $scope.meal;
+    };
+
+    $scope.resetCurrentMeal = function()
+    {
+        $scope.meal = {id: -1, name: null};
+    };
+
+    $scope.editMeal = function(meal)
+    {
+        $scope.meal = meal;
+        $scope.toggleModal();
+    };
+
+    $scope.saveMeal = function()
+    {
+        var meal = $scope.getCurrentMeal();
+        
+        // update view
+        if(meal.id < 0) {
+            $scope.meals.push(meal);
+        }
+
+        // update server & report on result
+        mealsService.saveMeal(meal).then(function(serviceResponse) {
+
+            $scope.toggleModal();
+
+            if(serviceResponse.success)
+            {
+                meal.id = serviceResponse.data.id;
+                $scope.resetCurrentMeal();
+                $scope.successMessage = "Success !";
+                $timeout(function(){$scope.successMessage = null;}, 2000);
+            }
+            else
+            {
+                $scope.errorMessage = serviceResponse.message;
+                $timeout(function(){$scope.errorMessage = null;}, 2000);
+            }
+        });
+    },
+
+    $scope.deleteMeal = function(meal)
+    {
+        // update view
+        var index = $scope.meals.indexOf(meal);
+        if(index > -1) {
+            $scope.meals.splice(index, 1);
+        }
+
+        // update server & report on result
+        mealsService.deleteMeal(meal).then(function(serviceResponse) {
+            if(serviceResponse.success)
+            {
+                $scope.successMessage = "Success !";
+                $timeout(function(){$scope.successMessage = null;}, 2000);
+            }
+            else
+            {
+                $scope.errorMessage = serviceResponse.message;
+                $timeout(function(){$scope.errorMessage = null;}, 2000);
+            }
+        });
+    };
+
+    // initial load
+    $scope.resetCurrentMeal();
+    $scope.getMeals();
+}])
+
+.factory('MealsSvc', function($http){
+    return {
+        getMeals : function(){
+            return $http.get('http://localhost:8080/meals').then(function(response){
+                return {success: false, message: null, data: response.data._embedded.meals};
+            },function(response){
+                return {success: false, message: response.statusText};
+            });
+        },
+        saveMeal : function(meal){
+
+            if(meal.id < 0)
+            {
+                return $http.post('http://localhost:8080/meals', meal).then(function(response){
+                    return {success: true, message: response.statusText, data: response.data};
+                },function(response){
+                    return {success: false, message: response.statusText};
+                });
+            }
+            else
+            {
+                return $http.put('http://localhost:8080/meals/'+meal.id, meal).then(function(response){
+                    return {success: true, message: response.statusText, data: response.data};
+                },function(response){
+                    return {success: false, message: response.statusText};
+                });
+            }
+        },
+        deleteMeal : function(meal){
+            return $http.delete('http://localhost:8080/meals/'+meal.id).then(function(response){
+                return {success: true, message: response.statusText};
+            },function(response){
+                return {success: false, message: response.statusText};
+            });
+        }
+    }
+});
